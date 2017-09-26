24baa3db586754c07b4f8b8318b6fc7c171ec58f
Judzhin Miles <info@msbios.com>
2017-09-26 13:18:07 +0300

    Lazy Composer Commiter

--- tests/ModuleTest.php
+++ tests/ModuleTest.php
@@ -3,14 +3,14 @@
  * @access protected
  * @author Judzhin Miles <info[woof-woof]msbios.com>
  */
-namespace MSBiosTest\CPanel;
+namespace MSBiosTest\View;
 
-use MSBios\CPanel\Module;
+use MSBios\View\Module;
 use PHPUnit\Framework\TestCase;
 
 /**
  * Class ModuleTest
- * @package MSBiosTest\Assetic
+ * @package MSBiosTest\View
  */
 class ModuleTest extends TestCase
 {
